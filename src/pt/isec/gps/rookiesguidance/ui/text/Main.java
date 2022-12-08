bb6b8996bef97b3795612867e5f14c19db35a517
Eduardo Correia <eduardo2002correia@gmail.com>
2022-12-08 22:01:49 +0000

    receba

--- src/pt/isec/gps/rookiesguidance/ui/text/Main.java
+++ src/pt/isec/gps/rookiesguidance/ui/text/Main.java
@@ -4,10 +4,9 @@ import javafx.application.Application;
 import pt.isec.gps.rookiesguidance.bd.ConnDB;
 import pt.isec.gps.rookiesguidance.ui.gui.MainJFX;
 
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
        Application.launch(MainJFX.class,args);
         //ConnDB cdb = new ConnDB();
 ////       if (cdb.removeRegisto(2019133920))
